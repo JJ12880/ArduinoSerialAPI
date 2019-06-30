30da9062e3ef342d92c47bf8aa1b5432c8a18ac2
JJ12880 <Justinj100@gmail.com>
2019-06-30 16:23:38 -0400

    init code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Aquaponics1
+{
+    public class Program
+    {
+        public static ManualResetEvent shutdown_Event = new ManualResetEvent(false);
+        public static void Main(string[] args)
+        {
+            SerialListener.init();
+            CreateWebHostBuilder(args).Build().Run();
+
+
+
+            shutdown_Event.Reset();
+            shutdown_Event.WaitOne();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ SerialListener.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Aquaponics1
+{
+    static class SerialListener
+    {
+        static SerialPort _serialPort;
+
+        public static void init()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM3";//Set your board COM
+            _serialPort.BaudRate = 9600;
+            _serialPort.Open();
+        }
+
+        public static JObject read_data()
+        {
+            return new JObject();
+
+            JObject data = new JObject();
+            string json = _serialPort.ReadExisting();
+            while (true)
+            {
+                if (json == "")
+                {
+                    Thread.Sleep(5000);
+                    continue;
+                }
+                try
+                {
+                    data = JObject.Parse(json);
+                }
+                catch
+                {
+                    continue;
+                }
+
+                return data;
+
+            }
+        }  
+    }
+}
