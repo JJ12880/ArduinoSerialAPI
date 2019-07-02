2bf81301edc718a803fd2bcbd20d0508b1ab29e0
JJ12880 <Justinj100@gmail.com>
2019-07-02 13:02:31 -0400

    Implement timer to reduce cpu overhead

--- SerialListener.cs
+++ SerialListener.cs
@@ -12,22 +12,29 @@ namespace Aquaponics1
         static SerialPort _serialPort;
 
         public static JObject data = new JObject();
-
+        internal static System.Timers.Timer TwoSecondTimer = new System.Timers.Timer(10000);
         public static void init()
         {
             _serialPort = new SerialPort();
             _serialPort.PortName = "COM3";//Set your board COM
             _serialPort.BaudRate = 9600;
             _serialPort.Open();
+            TwoSecondTimer.Elapsed += TwoSecondTimer_Elapsed;
+            TwoSecondTimer.Start();
+
         }
 
-        private static JObject read_data()
+        private static void TwoSecondTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
-           
+            read_data();
+        }
 
-           
+        private  static void read_data()
+        {
+
+            bool sucess = false;
             string json = _serialPort.ReadExisting();
-            while (true)
+            while (!sucess)
             {
                 if (json == "")
                 {
@@ -37,7 +44,7 @@ namespace Aquaponics1
                 try
                 {
                     data = JObject.Parse(json);
-                    Thread.Sleep(5000);
+                    sucess = true;
                 }
                 catch
                 {
